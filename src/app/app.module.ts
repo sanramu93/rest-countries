754c397ad5f988a1ca94d62294cf0555b6ff7e61
Santiago <sanramu93@gmail.com>
2022-06-09 17:55:16 -0500

    Add CSS responsive  grid to countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './pages/home/home.component';
+import { CountryCardComponent } from './country-card/country-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CountryCardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/country-card/country-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-card',
+  templateUrl: './country-card.component.html',
+  styleUrls: ['./country-card.component.css']
+})
+export class CountryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
