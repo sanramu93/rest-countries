42c30d94d7bb0933dafc3d9564e9f156fb0579bb
Santiago <sanramu93@gmail.com>
2022-06-10 17:46:39 -0500

    Add search button to search for country

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  darkMode = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDarkModeClick() {
+    this.darkMode = !this.darkMode;
   }
-
 }
